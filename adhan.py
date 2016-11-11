65ad701e30d53f6c873351bbf11d70ce926dc7c6
fcmam5 <dr.fcmam5@gmail.com>
2016-11-11 17:41:32 +0100

    move print into try block

--- adhan.py
+++ adhan.py
@@ -7,11 +7,11 @@ try:
     response = urlopen(request)
     answer = response.read()
     adhan_times = json.loads(answer)
+    print " Fajr "+adhan_times['data']['timings']['Fajr']+"\n",\
+        " Dhuhr "+adhan_times['data']['timings']['Dhuhr']+"\n",\
+        " Asr "+adhan_times['data']['timings']['Asr']+"\n",\
+        " Maghrib "+adhan_times['data']['timings']['Maghrib']+"\n",\
+        " Isha "+adhan_times['data']['timings']['Isha']+"\n",\
+
 except URLError, e:
     print "No responses, error"
-
-print " Fajr "+adhan_times['data']['timings']['Fajr']+"\n",\
-    " Dhuhr "+adhan_times['data']['timings']['Dhuhr']+"\n",\
-    " Asr "+adhan_times['data']['timings']['Asr']+"\n",\
-    " Maghrib "+adhan_times['data']['timings']['Maghrib']+"\n",\
-    " Isha "+adhan_times['data']['timings']['Isha']+"\n",\
